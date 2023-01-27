e568c765c37f761877e0e827555c87dd84242ab2
hcitlali <120482394+hcitlali@users.noreply.github.com>
2023-01-27 08:38:27 -0600

    Added a map, added choices, boolean, player input

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
-game.splash(game.askForNumber("Choose your character"), "1: Anya  2: Yor  3:Loid")
-let Yor = sprites.create(img`
-    . f f f . f f f f . f f f . 
-    f f f f f c c c c f f f f f 
-    f f f f b c c c c b f f f f 
-    f f f c 3 c c c c 3 c f f f 
-    . f 3 3 c c c c c c 3 3 f . 
-    . f c c c c 4 4 c c c c f . 
-    . f f c c 4 4 4 4 c c f f . 
-    . f f f b f 4 4 f b f f f . 
-    . f f 4 1 f d d f 1 4 f f . 
-    . . f f d d d d d d f f . . 
-    . . e f e 4 4 4 4 e f e . . 
-    . e 4 f b 3 3 3 3 b f 4 e . 
-    . 4 d f 3 3 3 3 3 3 c d 4 . 
-    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
-    . . . . f f f f f f . . . . 
-    . . . . f f . . f f . . . . 
-    `, SpriteKind.Player)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    game.showLongText("Hello  " + Name, DialogLayout.Top)
+    pause(200)
+    game.showLongText("Would you do a favor for me?", DialogLayout.Bottom)
+    pause(200)
+    game.showLongText("Gettin my treasure back", DialogLayout.Bottom)
+    pause(200)
+    story.showPlayerChoices("Accept", "Decline")
+    if (story.checkLastAnswer("Accept")) {
+    	
+    } else if (story.checkLastAnswer("Decline")) {
+        game.over(false)
+    }
+})
+let Name = ""
+Name = game.askForString("Name your character ")
+tiles.setCurrentTilemap(tilemap`level1`)
 let Anya = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
@@ -35,30 +33,27 @@ let Anya = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
+Anya.setScale(2, ScaleAnchor.Right)
+tiles.placeOnTile(Anya, tiles.getTileLocation(7, 15))
 let Loid = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . . . . f f f f . . . . 
+    . . f f e e e e f f . . 
+    . f f e e e e e e f f . 
+    f f f f 4 e e e f f f f 
+    f f f 4 4 4 e e f f f f 
+    f f f 4 4 4 4 e e f f f 
+    f 4 e 4 4 4 4 4 4 e 4 f 
+    f 4 4 f f 4 4 f f 4 4 f 
+    f e 4 d d d d d d 4 e f 
+    . f e d d b b d d e f . 
+    . f f e 4 4 4 4 e f f . 
+    e 4 f b 1 1 1 1 b f 4 e 
+    4 d f 1 1 1 1 1 1 f d 4 
+    4 4 f 6 6 6 6 6 6 f 4 4 
+    . . . f f f f f f . . . 
+    . . . f f . . f f . . . 
     `, SpriteKind.Player)
-if (Yor) {
-    controller.moveSprite(Yor)
-}
-if (Loid) {
-    controller.moveSprite(Loid)
-}
-if (Anya) {
-    controller.moveSprite(Anya)
-}
+Loid.setScale(2, ScaleAnchor.Middle)
+tiles.placeOnTile(Loid, tiles.getTileLocation(1, 3))
+scene.cameraFollowSprite(Anya)
+controller.moveSprite(Anya)
