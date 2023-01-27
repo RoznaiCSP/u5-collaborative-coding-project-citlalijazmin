0c640c03bc616bf4d38eb4dbe953f8eedbdc0d12
hcitlali <120482394+hcitlali@users.noreply.github.com>
2023-01-27 09:07:11 -0600

    added a map, boolean and choices

--- main.ts
+++ main.ts
@@ -1,22 +1,20 @@
-game.splash(game.askForNumber("Choose your character"), "1: Anya  2: Yor  3:Loid")
-let Yor = sprites.create(img`
-    . f f f . f f f f . f f f . 
-    f f f f f c c c c f f f f f 
-    f f f f b c c c c b f f f f 
-    f f f c 3 c c c c 3 c f f f 
-    . f 3 3 c c c c c c 3 3 f . 
-    . f c c c c 4 4 c c c c f . 
-    . f f c c 4 4 4 4 c c f f . 
-    . f f f b f 4 4 f b f f f . 
-    . f f 4 1 f d d f 1 4 f f . 
-    . . f f d d d d d d f f . . 
-    . . e f e 4 4 4 4 e f e . . 
-    . e 4 f b 3 3 3 3 b f 4 e . 
-    . 4 d f 3 3 3 3 3 3 c d 4 . 
-    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
-    . . . . f f f f f f . . . . 
-    . . . . f f . . f f . . . . 
-    `, SpriteKind.Player)
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Player, function (sprite, otherSprite) {
+    game.splash("Hello  " + Name)
+    pause(500)
+    game.showLongText("Would you do a kind favor for me?", DialogLayout.Bottom)
+    pause(500)
+    game.showLongText("To retrive my lost treasure", DialogLayout.Bottom)
+    pause(500)
+    story.showPlayerChoices("Accept :)", "Decline :(")
+    if (story.checkLastAnswer("Accept :)")) {
+    	
+    } else if (story.checkLastAnswer("Decline :(")) {
+        game.over(false)
+    }
+})
+let Name = ""
+Name = game.askForString("Name your character")
+tiles.setCurrentTilemap(tilemap`level1`)
 let Anya = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
@@ -35,30 +33,27 @@ let Anya = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
+Anya.setScale(2, ScaleAnchor.Right)
+tiles.placeOnTile(Anya, tiles.getTileLocation(7, 15))
 let Loid = sprites.create(img`
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
-    . . . . . . . . . . . . . . . . 
+    . f f f . f f f f . f f f . 
+    f f f f f c c c c f f f f f 
+    f f f f b c c c c b f f f f 
+    f f f c 3 c c c c 3 c f f f 
+    . f 3 3 c c c c c c 3 3 f . 
+    . f c c c c 4 4 c c c c f . 
+    . f f c c 4 4 4 4 c c f f . 
+    . f f f b f 4 4 f b f f f . 
+    . f f 4 1 f d d f 1 4 f f . 
+    . . f f d d d d d d f f . . 
+    . . e f e 4 4 4 4 e f e . . 
+    . e 4 f b 3 3 3 3 b f 4 e . 
+    . 4 d f 3 3 3 3 3 3 c d 4 . 
+    . 4 4 f 6 6 6 6 6 6 f 4 4 . 
+    . . . . f f f f f f . . . . 
+    . . . . f f . . f f . . . . 
     `, SpriteKind.Player)
-if (Yor) {
-    controller.moveSprite(Yor)
-}
-if (Loid) {
-    controller.moveSprite(Loid)
-}
-if (Anya) {
-    controller.moveSprite(Anya)
-}
+Loid.setScale(2, ScaleAnchor.Right)
+tiles.placeOnTile(Loid, tiles.getTileLocation(1, 3))
+scene.cameraFollowSprite(Anya)
+controller.moveSprite(Anya)
---
+++ tilemap.g.ts
@@ -0,0 +1,39 @@
+// Auto-generated code. Do not edit.
+namespace myTiles {
+    //% fixedInstance jres blockIdentity=images._tile
+    export const transparency16 = image.ofBuffer(hex``);
+
+    helpers._registerFactory("tilemap", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "level1":
+            case "level1":return tiles.createTilemap(hex`1000100001080908010801080908010809080108010801080108010801080108010801080909090909090909090909080108010804040404040404040403090801080109020202020202020202050908010801080202020202020202020509080108010802020202020202020205090801080108070707070702020202050908090801080909090909060202020509080108010801080108090602020205090801080108010801080906020202050908010801080908010809060202020509080108010901080109090602020205090809080108010801080906020202050908010801080109010809060202020509080108090801080108090602020205090809080108`, img`
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+`, [myTiles.transparency16,sprites.castle.tileGrass2,sprites.castle.tilePath5,sprites.castle.tilePath3,sprites.castle.tilePath2,sprites.castle.tilePath6,sprites.castle.tilePath4,sprites.castle.tilePath8,sprites.castle.tileGrass1,sprites.builtin.forestTiles0], TileScale.Sixteen);
+        }
+        return null;
+    })
+
+    helpers._registerFactory("tile", function(name: string) {
+        switch(helpers.stringTrim(name)) {
+            case "transparency16":return transparency16;
+        }
+        return null;
+    })
+
+}
+// Auto-generated code. Do not edit.
